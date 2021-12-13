66094ea5067ede155d4e5cdacb06dcaf2f2be8d7
alejandro.iran <kazawasaque@gmail.com>
2021-12-13 14:07:13 -0600

    App Movil

---
+++ Pruebasdos/AppShell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Pruebasdos.Views;
+using Xamarin.Forms;
+
+namespace Pruebasdos
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
